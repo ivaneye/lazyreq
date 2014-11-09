653e4cbe9b220ad1946e527eac9e29622b0edbbc
ivanpig <ivan.pig@foxmail.com>
2014-11-09 21:17:21 +0800

    优化程序运行

--- src/lazyreq/core/handler.clj
+++ src/lazyreq/core/handler.clj
@@ -5,15 +5,16 @@
             [clojure.string :as str]
             [cronj.core :as c]
             [lazyreq.util.loader :refer :all]
+            [clojure.java.io :as io]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
-(def mapping-cfg (:mappings cfg))
-
-(defn- do-require [s]
+(defn do-require [s]
   (if (vector? s)
     (doseq [p s]
       (require [(symbol p)]))
     (require [(symbol s)])))
+
+(def mapping-cfg (:mappings cfg))
 
 (doseq [c mapping-cfg]
   (do-require (:core c))
@@ -27,20 +28,50 @@
     (recur (rest funs) ((first funs) args))
     args))
 
+(defn resolve-core [cfg]
+  (resolve (symbol (str (:core cfg) "/core"))))
+
+(def resolve-core-mem (memoize resolve-core))
+
+(defn resolve-cpre [cfg]
+  (map #(resolve (symbol (str % "/pre"))) (:clojure-pres cfg)))
+
+(def resolve-cpre-mem (memoize resolve-cpre))
+
+(defn resolve-clj [s args k cfg]
+  (map #(partial (resolve (symbol (str % s))) args) (k cfg)))
+
+(def resolve-clj-mem (memoize resolve-clj))
+
+(defn resolve-jpres [cfg]
+  (map #(fn [x] (.pre (.newInstance (resolve (symbol %))) x)) (:java-pres cfg)))
+
+(def resolve-jpres-mem (memoize resolve-jpres))
+
+(defn resolve-jerrs [args cfg]
+  (map #(partial (fn [a b] (.err (.newInstance (resolve (symbol %))) a b)) args) (:java-errs cfg)))
+
+(def resolve-jerrs-mem (memoize resolve-jerrs))
+
+(defn resolve-jposts [args cfg]
+  (map #(partial (fn [a b] (.post (.newInstance (resolve (symbol %))) a b)) args) (:java-posts cfg)))
+
+(def resolve-jposts-mem (memoize resolve-jposts))
+
 (defn wrap-invoke [cfg args]
-  (let [core (resolve (symbol (str (:core cfg) "/core")))
-        pres (map #(resolve (symbol (str % "/pre"))) (:clojure-pres cfg))
-        errs (map #(partial (resolve (symbol (str % "/err"))) args) (:clojure-errs cfg))
-        jpres (map #(fn [x] (.pre (.newInstance (resolve (symbol %))) x)) (:java-pres cfg))
-        jerrs (map #(partial (fn [a b] (.err (.newInstance (resolve (symbol %))) a b)) args) (:java-errs cfg))
+  (let [core (resolve-core-mem cfg)
+        pres (resolve-cpre-mem cfg)
+        errs (resolve-clj-mem "/err" args :clojure-errs cfg)
+        jpres (resolve-jpres-mem cfg)
+        jerrs (resolve-jerrs-mem args cfg)
         invoke-pres (concat pres jpres)
         invoke-errs (concat errs jerrs)]
     (try
       (let [param (invoke invoke-pres args)
-            posts (map #(partial (resolve (symbol (str % "/post"))) param) (:clojure-posts cfg))
-            jposts (map #(partial (fn [a b] (.post (.newInstance (resolve (symbol %))) a b)) param) (:java-posts cfg))
-            inner-errs (map #(partial (resolve (symbol (str % "/err"))) param) (:clojure-errs cfg))
-            inner-jerrs (map #(partial (fn [a b] (.err (.newInstance (resolve (symbol %))) a b)) param) (:java-errs cfg))
+            posts (resolve-clj-mem "/post" param :clojure-posts cfg)
+            jposts (resolve-jposts-mem param cfg)
+            inner-errs (resolve-clj-mem "/err" param :clojure-errs cfg)
+            inner-jerrs (resolve-jerrs-mem param cfg)
             invoke-posts (concat posts jposts)
             invoke-inner-errs (concat inner-errs inner-jerrs)]
         (try (invoke invoke-posts (core param))
@@ -50,12 +81,8 @@
       (catch Exception e
         (invoke invoke-errs e)))))
 
-
-(defn invoke-task [fun]
-  (fun))
-
 ;日终
-(def cj (c/cronj :entries (vec (map #(invoke-task (resolve (symbol (str % "/task")))) (:tasks cfg)))))
+(def cj (c/cronj :entries (vec (map #((resolve (symbol (str % "/task")))) (:tasks cfg)))))
 
 (defn recall []
   (c/start! cj))
@@ -78,10 +105,7 @@
   (ANY (:url arg) request
         (wrap-invoke arg request)))
 
-(defmacro functionize [macro]
-  `(fn [& args#] (eval (cons '~macro (cons 'app-routes args#)))))
-
-(apply (functionize defroutes) (map method mapping-cfg))
+(def app-routes (apply routes (map method mapping-cfg)))
 
 (def app
   (wrap-defaults app-routes {}))
